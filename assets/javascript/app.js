3e65f3b524fb2238616564c4aef9722b48e49a55
da Vinci <brucewilson1103@gmail.com>
2018-12-20 09:43:37 -0500

    fixed variable bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,18 @@ var numcorrect = 0
 // var numwrong = 0
 
 function QuizTimer() {
-  document.getElementById("timer").innerHTML = i--;
+  document.getElementById("timer").innerHTML = "Time Left: " + i--;
 
   $('input[name=q1]:checked').val();
   console.log($('input[name=q1]:checked').val())
 
   if (i < 0) {
-    clearInterval(myVar);
+    clearInterval(myVar) ;
+    Timer();
 
     if (
       $('input[name=q1]:checked').val() === "Walkers") {
       numcorrect++;
-
     }
 
     if (
@@ -28,13 +28,11 @@ function QuizTimer() {
     if (
       $('input[name=q3]:checked').val() === "Crossbow") {
       numcorrect++;
-
     }
 
     if (
       $('input[name=q4]:checked').val() === "Hilltop") {
       numcorrect++;
-
     }
 
     if (
@@ -50,12 +48,48 @@ function QuizTimer() {
   }
   // if timer runs out then check for correct answers. Then I will display in a div on the page how many questions are correct and how many questions are incorrect . 
 
+    $(".results").one('click', function (){
+      clearInterval(myVar) ;
+        console.log(numcorrect);
+        numcorrect=0;
+      if (
+        $('input[name=q1]:checked').val() === "Walkers") {
+        numcorrect++;
+  
+      }
+        console.log(numcorrect);
+      if (
+        parseInt($('input[name=q2]:checked').val()) === 2) {
+        numcorrect++;
+       }
+  
+      if (
+        $('input[name=q3]:checked').val() === "Crossbow") {
+        numcorrect++;
+  
+      }
+  
+      if (
+        $('input[name=q4]:checked').val() === "Hilltop") {
+        numcorrect++;
+  
+      }
+  
+      if (
+        $('input[name=q5]:checked').val() === "Negan") {
+        numcorrect++;
 
+      }
+      console.log(numcorrect);
+      $(".correctoutput").text("Number Correct: " + numcorrect);
+  
+      $(".wrongoutput").text("Number Incorrect: " + (5 - numcorrect));
+    });
 
 }
 
 
-// function Timer()
-//     {
-//          alert("You are out of time!");
-//     }
+function Timer()
+    {
+         alert("You are out of time!");
+    }
