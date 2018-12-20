0e872b8d474ffcef89ec2dc99983b6602fdd326c
da Vinci <brucewilson1103@gmail.com>
2018-12-19 20:01:29 -0500

    questions added everything functional needs formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var myVar = setInterval(function() {
+var myVar = setInterval(function () {
   QuizTimer()
 }, 1000);
 var i = 10;
@@ -13,26 +13,43 @@ function QuizTimer() {
 
   if (i < 0) {
     clearInterval(myVar);
+
     if (
-    $('input[name=q1]:checked').val() === 2 ){
-    numcorrect ++;
+      $('input[name=q1]:checked').val() === "Walkers") {
+      numcorrect++;
 
     }
 
     if (
-    parseInt($('input[name=q2]:checked').val()) === 2 ){
-    numcorrect ++;
-
-    
+      parseInt($('input[name=q2]:checked').val()) === 2) {
+      numcorrect++;
     }
-    $(".correctoutput").text(numcorrect);
 
-    $(".wrongoutput").text(5 - numcorrect);
+    if (
+      $('input[name=q3]:checked').val() === "Crossbow") {
+      numcorrect++;
+
+    }
+
+    if (
+      $('input[name=q4]:checked').val() === "Hilltop") {
+      numcorrect++;
+
+    }
+
+    if (
+      $('input[name=q5]:checked').val() === "Negan") {
+      numcorrect++;
+
+    }
+    $(".correctoutput").text("Number Correct: " + numcorrect);
+
+    $(".wrongoutput").text("Number Incorrect: " + (5 - numcorrect));
     console.log(numcorrect)
 
   }
-    // if timer runs out then check for correct answers. Then I will display in a div on the page how many questions are correct and how many questions are incorrect . 
-    
+  // if timer runs out then check for correct answers. Then I will display in a div on the page how many questions are correct and how many questions are incorrect . 
+
 
 
 }
